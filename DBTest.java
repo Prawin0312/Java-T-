6bd19ada3b034d77c0d7a2d3727ad1c426ddbc2b
Praveen <59218233+Prawin0312@users.noreply.github.com>
2019-12-31 01:23:07 -0800

    Add files via upload

--- DBTest.java
+++ DBTest.java
@@ -6,16 +6,24 @@ public class DBTest
 	{
 		String url="jdbc:oracle:thin:@localhost:1521:ORCL";
 		Connection con=null;
-		Statement st = null;
-		ResultSet rs =null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
 		try
 		{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		con = DriverManager.getConnection(url,"scott","tiger");
-		st = con.createStatement();
-		rs = st.executeQuery("select * from emp");
+		st = con.prepareStatement("select * from dept");
+		rs = st.executeQuery();
+		ResultSetMetaData rsmd= rs.getMetaData();
+		int count= rsmd.getColumnCount();
+		for(int i=1;i<+count;i++)
+			System.out.println(rsmd.getColumnName(i));
 		while(rs.next())
-			System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+		{
+			for(int i=1;i<count;i++)
+				System.out.println(rs.getString(i)+" ");
+			System.out.println();
+		}
 		}
 		catch(Exception e)
 		{
