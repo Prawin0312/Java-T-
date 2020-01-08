6422a6583d7b7c38aff7ebe0fa5a6582125ad267
Praveen <59218233+Prawin0312@users.noreply.github.com>
2020-01-07 19:29:00 -0800

    hibernate with employee,deparment examples

--- App.java
+++ App.java
@@ -1,34 +1,73 @@
-package com.deloitte.firstmvn.hibfirst;
+package com.deloitte.thirdmvn.hibthird;
 import java.sql.Date;
-import java.util.Properties;
+import java.util.ArrayList;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import com.deloitte.firstmvn.hibfirst.entity.Employee;
+
+import com.deloitte.thirdmvn.hibthird.entity.Aadharcard;
+import com.deloitte.thirdmvn.hibthird.entity.Department;
+import com.deloitte.thirdmvn.hibthird.entity.Employee;
+import com.deloitte.thirdmvn.hibthird.entity.Person;
+
 public class App 
 {
-    public static void main(String[] args )
+    public static void main( String[] args )
     {
-    	Properties prop= new Properties();
-    	prop.put("hibernate.connection.driver_class","oracle.jdbc.driver.OracleDriver");
-    	prop.put("hibernate.connection.url","jdbc:oracle:thin:@localhost:1521:orcl");
-    	prop.put("hibernate.connection.username","scott");
-    	prop.put("hibernate.connection.password","tiger");
-    	prop.put("hibernate.dialect","org.hibernate.dialect.Oracle10gDialect");
-    	prop.put("hibernate.show_sql","true");
-        Configuration cfg=new Configuration();
-        cfg.addAnnotatedClass(Employee.class);
-        cfg.addProperties(prop);
-        SessionFactory factory=cfg.buildSessionFactory();
-        Session session = factory.openSession();
-        Employee e = session.load(Employee.class,101);
-        //if(e!=null)
-        	System.out.println(e.getName()+"  "+e.getSalary()+"  "+e.getDoj());
-       //Transaction trans = session.beginTransaction();
-       //session.delete(e);
-       //trans.commit();
-        session.close();
-        factory.close();        
+      Configuration cfg = new Configuration();
+      cfg.configure();
+      SessionFactory factory=cfg.buildSessionFactory();
+      Session session = factory.openSession();
+      //Aadharcard ac= new Aadharcard(7001,"Govt. of India",new Date(1999,10,10));
+      //Person p1= new Person("Ajay",26,"Hitc city",ac);
+      //Transaction trans= session.beginTransaction();
+      /*Person p1= session.get(Person.class,"Ajay");
+      if(p1!=null)
+      {
+    	  System.out.println(p1.getAddress()+" "+p1.getAge());
+    	  Aadharcard ac= p1.getAcard();
+    	  System.out.println(ac.getAid()+" "+ac.getIssueAuth()+" "+ac.getIssueDate());
+      }
+      Aadharcard ac1=session.get(Aadharcard.class, 7001);
+      if(ac1!=null)
+      {
+    	  System.out.println(ac1.getIssueAuth()+" "+ac1.getIssueDate());
+    	  Person p= ac1.getPerson();
+    	  System.out.println(p.getName()+" "+p.getAddress()+" "+p.getAge());
+      }
+      //session.save(ac);
+      //session.save(p1);
+      //trans.commit();*/
+     
+      
+       
+      /*Employee e1= new Employee(101,"Ajay",1000);
+      Employee e2= new Employee(102,"Vijay",12000);
+      Employee e3= new Employee(103,"Uday",92000);
+      Employee e4= new Employee(104,"Vinay",10000);
+      ArrayList<Employee> el1= new ArrayList<Employee>();
+      ArrayList<Employee> el2=new ArrayList<Employee>();
+      el1.add(e1);
+      el2.add(e2);
+      el1.add(e3);
+      el2.add(e4);
+      Department d1= new Department(10,"Sales",el1);
+      Department d2 = new Department(20,"Markerting",el2);
+      session.save(e1); session.save(e2); session.save(e3);
+      session.save(e4); session.save(d1); session.save(d2);
+      session.beginTransaction().commit();*/
+      
+      
+      Department d= session.get(Department.class,20);
+      if(d!=null)
+      {
+    	  System.out.println(d.getDname()+" "+d.getDeptno());
+    	  for(Employee e: d.getElist())
+    		  System.out.println(e.getEmpid()+" "+e.getEmpname()+" "+e.getSalary());
+      }
+      session.close();
+      factory.close();
     }
 }
--- Employee.java
+++ Employee.java
@@ -1,35 +1,38 @@
-package com.deloitte.firstmvn.hibsecond.entity;
-import javax.persistence.DiscriminatorColumn;
+package com.deloitte.thirdmvn.hibthird.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 @Entity
-@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
-@DiscriminatorColumn(name="type")
-public abstract class Employee
+public class Employee 
 {
-  @Id
-  int empid;
-  String empname;
-  public Employee(){}
-  public Employee(int empid, String empname) {
-	
-	this.empid = empid;
-	this.empname = empname;
-	
-}
-public int getEmpid() {
-	return empid;
-}
-public void setEmpid(int empid) {
-	this.empid = empid;
-}
-public String getEmpname() {
-	return empname;
-}
-public void setEmpname(String empname) {
-	this.empname = empname;
-}
-  
+    @Id
+    int empid;
+    String empname;
+    double salary;
+	public Employee() {
+		super();
+	}
+	public Employee(int empid, String empname, double salary) {
+		this.empid = empid;
+		this.empname = empname;
+		this.salary = salary;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+    
 }
