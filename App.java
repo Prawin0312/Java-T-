f0547b619380a52eaf7ee5af5f69567292190057
Praveen <59218233+Prawin0312@users.noreply.github.com>
2020-01-07 19:41:33 -0800

    hibernate third app.java

--- App.java
+++ App.java
@@ -1,22 +1,24 @@
 package com.deloitte.thirdmvn.hibthird;
 import java.sql.Date;
 import java.util.ArrayList;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
 import com.deloitte.thirdmvn.hibthird.entity.Aadharcard;
+import com.deloitte.thirdmvn.hibthird.entity.Courses;
 import com.deloitte.thirdmvn.hibthird.entity.Department;
 import com.deloitte.thirdmvn.hibthird.entity.Employee;
 import com.deloitte.thirdmvn.hibthird.entity.Person;
+import com.deloitte.thirdmvn.hibthird.entity.Student;
 
 public class App 
 {
     public static void main( String[] args )
     {
       Configuration cfg = new Configuration();
+      cfg.addAnnotatedClass(Courses.class);
+      cfg.addAnnotatedClass(Student.class);
       cfg.configure();
       SessionFactory factory=cfg.buildSessionFactory();
       Session session = factory.openSession();
@@ -54,18 +56,48 @@ public class App
       el1.add(e3);
       el2.add(e4);
       Department d1= new Department(10,"Sales",el1);
-      Department d2 = new Department(20,"Markerting",el2);
+      Department d2 = new Department(20,"Marketing",el2);
       session.save(e1); session.save(e2); session.save(e3);
       session.save(e4); session.save(d1); session.save(d2);
       session.beginTransaction().commit();*/
       
       
-      Department d= session.get(Department.class,20);
+     /* Department d= session.get(Department.class,20);
       if(d!=null)
       {
     	  System.out.println(d.getDname()+" "+d.getDeptno());
     	  for(Employee e: d.getElist())
     		  System.out.println(e.getEmpid()+" "+e.getEmpname()+" "+e.getSalary());
+      }*/
+      /*Department d = session.get(Department.class,20);
+      session.delete(d);
+      session.beginTransaction().commit();*/
+      /*Department d = session.get(Department.class,20);
+      if(d!=null)
+      {
+    	  System.out.println(d.getDeptno()+" "+d.getDname());
+    	  for(Employee e: d.getElist())
+    		  System.out.println(e.getEmpid()+" "+e.getEmpname()+" "+e.getSalary());
+    	  Employee e = session.get(Employee.class,101);
+    	  if(e!=null)
+    	  {
+    		  System.out.println(e.getEmpname()+" "+e.getSalary()); 
+    		  System.out.println(e.getDept().getDeptno()+" "+e.getDept().getDname());
+    	  }
+      }*/
+      Courses c = session.get(Courses.class, 7001);
+      if(c!=null)
+      {
+    	  System.out.println(c.getCname()+" "+c.getDuration());
+    	  for(Student s:c.getSlist())
+    		  System.out.println(s.getSid()+" "+s.getSname()+" "+s.getAddress());
+      }
+      Student s= session.get(Student.class,102);
+      if(s!=null)
+      {
+    	  System.out.println(s.getSname()+" "+s.getAddress());
+    	  for(Courses c1:s.getClist())
+             System.out.println(c1.getCid()+" "+ c1.getCname()+" "+c1.getDuration());  	  
       }
       session.close();
       factory.close();
